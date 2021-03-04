edcbb10befc7a5bf0fd76024e9daa82f3a5840bb
Arzanis <sergey_asov@mail.ru>
2021-03-04 23:15:29 +0500

    settings fix for dev

--- Models/AppSettings.cs
+++ Models/AppSettings.cs
@@ -2,11 +2,11 @@
 {
     public static class AppSettings
     {
-        public static string AppUrl { get; set; } = "https://arzanis.me/{0}";
+        public static string AppUrl { get; set; } = "https://www.arzanis.me/{0}";
 
         public static string BotName { get; set; } = "FuckingAdviserBot";
         public static string TgApiKey { get; set; } = "1339776940:AAEDCw--99Y-GVxeGADagGj5bdZGWq9-FMY";
-        public static bool DebugFlag { get; set; } = true;
+        public static bool DebugFlag { get; set; } = false;
 
         public static int DebugChatId { get; set; } = -590873509;
 
